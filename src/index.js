b1c8b37854bccdf45e8e8ab50264e2ff59f9e2fb
hautai <hautai34@gmail.com>
2020-08-10 21:13:10 +0800

    add index method

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
-Class Combination {
+class Combination {
   constructor() {
-    this.order = [];
-    arguments.forEach((arr, i) => {
-      const name = `_${i}`;
-      this[name] = arr;
-      this.order[i] = name;
+    this.lens = [];
+    this.totalLen = 0;
+    this.arrs = arguments;
+
+    [ ...arguments ].forEach((arr, i) => {
+      const arrLen = arr.length;
+      this.lens[i] = arrLen
+      this.totalLen += arrLen;
     });
+
+    this.rLens = [ ...this.lens ].reverse();
+    this.rArrs = [ ...this.arrs ].reverse();
+    this.srArrs = this.rArrs.map(arr => arr.unshift(arr.pop()));
+  }
+
+  index(n) {
+    n = n + 1;
+    const srArrs = this.rArrs;
+    const result = [];
+    this.rLens.reduce((n, d, i) => {
+      result[i] = srArrs[i][n % d];
+      return Math.ceil(n / d);
+    }, n);
+    return result.reverse();
   }
 }
 
 module.exports = Combination;
+
+/*
+  combine() {
+    const arrs = this.arrs;
+    const lens = this.lens;
+    const totalLen = this.totalLen;
+    const result = new Array(totalLen);
+
+    for (const i = 0; i < totalLen; i++) {
+
+    }
+  }
+  */
