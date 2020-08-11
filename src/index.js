9147b0c83dc3e207f71e74e4558107ecc9cc0f1c
hautai <hautai34@gmail.com>
2020-08-11 11:58:22 +0800

    update logic

--- src/index.js
+++ src/index.js
@@ -1,42 +1,57 @@
 class Combination {
   constructor() {
-    const args = [ ...arguments ];
-    this.sArrs = args.map(arr => {
-      arr.unshift(arr.pop());
-      return arr;
-    });
+    this.arrs = [ ...arguments ];
     this.divisors = [];
 
-    const totalLen = args.reverse().reduce((divisor, arr) => {
+    this.totalLen = [ ...arguments ].reverse().reduce((divisor, arr) => {
       this.divisors.unshift(divisor);
       return divisor * arr.length;
     }, 1);
-
-    this.totalLen = totalLen;
   }
 
   index(n) {
-    n = n + 1;
-    console.log(this.sArrs);
     const divisors = this.divisors;
-    return this.sArrs.reduce((result, arr, i) => {
-      result[i] = arr[Math.ceil(n / divisors[i]) % arr.length];
+
+    return this.arrs.reduce((result, arr, i) => {
+      result[i] = arr[Math.floor(n / divisors[i]) % arr.length];
+
       return result;
     }, []);
   }
+/*
+  combine() {
+    const divisors = this.divisors;
+    const sArrs = this.sArrs;
+    const sArrsLen = sArrs.length;
+    const totalLen = this.totalLen;
+    const results = Array(totalLen).fill([]);
+
+    for (let s = 0; s < sArrsLen; s++) {
+      const sArr = sArrs[s];
+      const sArrLen = sArr.length;
+      const divisor = divisors[s];
+      //console.log(sArr);
+
+      for (let n = 0; n < totalLen; n ++) {
+        //console.log(n, divisor, s);
+        results[n][s] = sArr[Math.floor(n / divisor) % sArrLen];
+        //console.log(results);
+      }
+    }
+    return results
+  }
+  */
+}
+
+const arr1 = ['A', 'B', 'C'];
+const arr2 = ['a', 'b', 'c'];
+const arr3 = ['0', '1'];
+
+const test = new Combination(arr1, arr2, arr3);
+//const result = test.combine();
+
+for (let i = 0; i < 18; i++) {
+  console.log(test.index(i));
 }
 
 module.exports = Combination;
-
-/*
-  combine() {
-    const arrs = this.arrs;
-    const lens = this.lens;
-    const totalLen = this.totalLen;
-    const result = new Array(totalLen);
-
-    for (const i = 0; i < totalLen; i++) {
-
-    }
-  }
-  */
